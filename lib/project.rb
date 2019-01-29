a677254b8a32594edb614dcd8947ccecf6d4b93c
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-29 04:40:04 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,6 +1,15 @@
 class Project
-attr_accessor :title
+attr_accessor :title, :backers 
     def initialize(title)
       @title = title
     end
+def add_backer(name)
+  @backers= []
+  @backers <<name
+  
+end
+
+
+
+    
 end
