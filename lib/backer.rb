f033d8c6531f50f17284504746834f231b3a841e
Sim Greenbaum <gbs4ever2@gmail.com>
2019-01-29 04:44:54 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,14 +1,15 @@
 class Backer
 
-attr_reader :name,   :backed_projects
+attr_accessor :name,   :backed_projects
     def initialize(name)
       @name = name
+      @backed_projects= []
     end
 
 
 
     def back_project(project)
-      @backed_projects= []
+
       @backed_projects << project
     end
 end
